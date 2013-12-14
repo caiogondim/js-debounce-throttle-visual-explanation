268ddbf22dcf22b66fa84cad26f7b0ffb4b7ecd4
Caio Gondim <me@caiogondim.com>
2013-12-14 01:40:00 -0200

    Lints JS, just for fun

--- gruntfile.js
+++ gruntfile.js
@@ -1,28 +1,38 @@
-module.exports = function (grunt) {
-  'use strict';
+/* global module */
 
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
+module.exports = function (grunt) {
+  "use strict";
+
+  grunt.loadNpmTasks("grunt-contrib-connect")
+  grunt.loadNpmTasks("grunt-contrib-csslint")
+  grunt.loadNpmTasks("grunt-contrib-jshint")
 
   grunt.initConfig({
     connect: {
       dev: {
         options: {
           port: 3000,
-          base: '.',
+          base: ".",
           keepalive: true
         }
       }
     },
     csslint: {
       options: {
-        'universal-selector': false,
-        'box-sizing': false
+        "universal-selector": false,
+        "box-sizing": false
       },
-      src: ['./*.css']
-    }
-  });
+      src: ["./*.css"]
+    },
+    jshint: {
+      files: [
+        "Gruntfile.js",
+        "scripts/maini.js"
+      ],
+      options: grunt.file.readJSON(".jshintrc")
+    },
+  })
 
-  grunt.registerTask('run', ['connect:dev']);
-  grunt.registerTask('test', ['csslint']);
-};
+  grunt.registerTask("run", ["connect:dev"])
+  grunt.registerTask("test", ["csslint", "jshint"])
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,19 @@
-var exp = (function() {
-  'use strict'
+/* global $, document, setTimeout, setInterval, clearInterval, console,
+   clearTimeout */
+
+(function () {
+  "use strict";
 
   var exp = {
-    init: function() {
+    init: function () {
       this.TIME_INTERVAL = 200
       this.ITERATION_MAX = 35
 
       this.dom = {}
-      this.dom.exp = $('.exp')
-      this.dom.vanillaEvent = this.dom.exp.find('.exp__item--vanilla')
-      this.dom.debouncedEvent = this.dom.exp.find('.exp__item--debounce')
-      this.dom.throttledEvent = this.dom.exp.find('.exp__item--throttle')
+      this.dom.exp = $(".exp")
+      this.dom.vanillaEvent = this.dom.exp.find(".exp__item--vanilla")
+      this.dom.debouncedEvent = this.dom.exp.find(".exp__item--debounce")
+      this.dom.throttledEvent = this.dom.exp.find(".exp__item--throttle")
 
       this.bindEvents()
       this.consoleLocalOnly()
@@ -18,7 +21,7 @@ var exp = (function() {
       return this
     },
 
-    bindEvents: function() {
+    bindEvents: function () {
       this.onMouseEnterExp()
       this.onMouseLeaveExp()
       this.onMouseMoveExp()
@@ -26,7 +29,7 @@ var exp = (function() {
       this.onMouseLeaveBody()
     },
 
-    draw: function() {
+    draw: function () {
       if (!this.draw.iterationCount) {
         this.draw.iterationCount = 0
       }
@@ -34,8 +37,8 @@ var exp = (function() {
       this.wasMouseMoveVanillaTriggered = false
       this.wasMouseMoveDebouncedTriggered = false
       this.wasMouseMoveThrottledTriggered = false
-      console.log('draw setInterval')
-      this.draw.interval = setInterval(function() {
+      console.log("draw setInterval")
+      this.draw.interval = setInterval(function () {
 
         if (this.draw.iterationCount >= this.ITERATION_MAX) {
           this.reset()
@@ -43,32 +46,32 @@ var exp = (function() {
 
         if (this.wasMouseMoveVanillaTriggered) {
           this.dom.vanillaEvent
-            .find('.exp__rail')
-            .append('<span class="exp__tic-fired exp__tic-fired--vanilla"></span>')
+            .find(".exp__rail")
+            .append("<span class=\"exp__tic-fired exp__tic-fired--vanilla\"></span>")
         } else {
           this.dom.vanillaEvent
-            .find('.exp__rail')
-            .append('<span class="exp__tic exp__tic--vanilla"></span>')
+            .find(".exp__rail")
+            .append("<span class=\"exp__tic exp__tic--vanilla\"></span>")
         }
 
         if (this.wasMouseMoveDebouncedTriggered) {
           this.dom.debouncedEvent
-            .find('.exp__rail')
-            .append('<span class="exp__tic-fired exp__tic-fired--debounce"></span>')
+            .find(".exp__rail")
+            .append("<span class=\"exp__tic-fired exp__tic-fired--debounce\"></span>")
         } else {
           this.dom.debouncedEvent
-            .find('.exp__rail')
-            .append('<span class="exp__tic exp__tic--debounce"></span>')
+            .find(".exp__rail")
+            .append("<span class=\"exp__tic exp__tic--debounce\"></span>")
         }
 
         if (this.wasMouseMoveThrottledTriggered) {
           this.dom.throttledEvent
-            .find('.exp__rail')
-            .append('<span class="exp__tic-fired exp__tic-fired--throttle"></span>')
+            .find(".exp__rail")
+            .append("<span class=\"exp__tic-fired exp__tic-fired--throttle\"></span>")
         } else {
           this.dom.throttledEvent
-            .find('.exp__rail')
-            .append('<span class="exp__tic exp__tic--throttle"></span>')
+            .find(".exp__rail")
+            .append("<span class=\"exp__tic exp__tic--throttle\"></span>")
         }
 
         this.wasMouseMoveVanillaTriggered = false
@@ -79,36 +82,36 @@ var exp = (function() {
       }.bind(this), this.TIME_INTERVAL)
     },
 
-    reset: function() {
-      console.log('reset')
+    reset: function () {
+      console.log("reset")
       this.draw.iterationCount = 0
-      this.dom.exp.find('.exp__rail *').hide()
+      this.dom.exp.find(".exp__rail *").hide()
     },
 
-    onMouseEnterExp: function() {
-      this.dom.exp.on('mouseenter', function(event) {
-        console.log('mouse enter triggered')
+    onMouseEnterExp: function () {
+      this.dom.exp.on("mouseenter", function (event) {
+        console.log("mouse enter triggered")
         this.draw()
       }.bind(this))
     },
 
-    onMouseLeaveExp: function() {
-      this.dom.exp.on('mouseleave', function(event) {
-        console.log('mouse leave triggered')
+    onMouseLeaveExp: function () {
+      this.dom.exp.on("mouseleave", function (event) {
+        console.log("mouse leave triggered")
         clearInterval(this.draw.interval)
       }.bind(this))
     },
 
-    onMouseMoveExp: function() {
-      this.dom.exp.on('mousemove', function(event) {
-        console.log('mouse move triggered')
+    onMouseMoveExp: function () {
+      this.dom.exp.on("mousemove", function (event) {
+        console.log("mouse move triggered")
         this.handleVanillaMouseMove()
         this.handleDebouncedMouseMove()
         this.handleThrottledMouseMove()
       }.bind(this))
     },
 
-    handleVanillaMouseMove: function() {
+    handleVanillaMouseMove: function () {
       this.wasMouseMoveVanillaTriggered = true
     },
 
@@ -117,72 +120,72 @@ var exp = (function() {
       var timeout = null
 
       var debouncedEvent = function (args) {
-        console.log('mouse move debounced triggered')
+        console.log("mouse move debounced triggered")
         this.wasMouseMoveDebouncedTriggered = true
       }
 
-      return function() {
+      return function () {
         var context = this
         var args = arguments
         clearTimeout(timeout)
-        timeout = setTimeout(function(){
+        timeout = setTimeout(function () {
           debouncedEvent.apply(context, args)
         }, timeWindow)
       }
     }()),
 
     handleThrottledMouseMove: (function () {
-        var timeWindow = 400
-        var lastExecution = new Date((new Date()).getTime() - timeWindow)
+      var timeWindow = 400
+      var lastExecution = new Date((new Date()).getTime() - timeWindow)
 
-        var throttledEvent = function () {
-          console.log('mouse move throttled triggered')
-          this.wasMouseMoveThrottledTriggered = true
-        }
+      var throttledEvent = function () {
+        console.log("mouse move throttled triggered")
+        this.wasMouseMoveThrottledTriggered = true
+      }
 
-        return function() {
-          if ((lastExecution.getTime() + timeWindow) <= (new Date()).getTime()) {
-            lastExecution = new Date()
-            return throttledEvent.bind(this)()
-          }
+      return function () {
+        if ((lastExecution.getTime() + timeWindow) <= (new Date()).getTime()) {
+          lastExecution = new Date()
+          return throttledEvent.bind(this)()
         }
+      }
     }()),
 
-    onMouseEnterModal: function() {
+    onMouseEnterModal: function () {
       this.onMouseEnterModal.timeout = null
 
-      $('.modal').on('mouseenter', function(event) {
-        $('.modal').addClass('modal--hidden')
-        $('.exp').removeClass('exp--hidden')
+      $(".modal").on("mouseenter", function (event) {
+        $(".modal").addClass("modal--hidden")
+        $(".exp").removeClass("exp--hidden")
 
-        this.onMouseEnterModal.timeout = setTimeout(function(){
-          $('.modal').css('z-index', '0')
+        this.onMouseEnterModal.timeout = setTimeout(function () {
+          $(".modal").css("z-index", "0")
         }.bind(this), 1000)
       }.bind(this))
     },
 
-    onMouseLeaveBody: function() {
-      $('body').on('mouseleave', function(event) {
+    onMouseLeaveBody: function () {
+      $("body").on("mouseleave", function (event) {
         clearTimeout(this.onMouseEnterModal.timeout)
 
-        $('.modal')
-          .removeClass('modal--hidden')
-          .css('z-index', '2')
+        $(".modal")
+          .removeClass("modal--hidden")
+          .css("z-index", "2")
 
-        $('.exp')
-          .addClass('exp--hidden')
+        $(".exp")
+          .addClass("exp--hidden")
       }.bind(this))
     },
 
-    consoleLocalOnly: function() {
+    consoleLocalOnly: function () {
       // monkey patch to run console.log only in localhost during development
 
-      if (document.location.href.indexOf('://localhost') === -1) {
-        console.log = function() {}
+      if (document.location.href.indexOf("://localhost") === -1) {
+        console.log = function () {}
       }
     }
   }
 
-  return exp.init()
+  exp.init()
 
 }())
